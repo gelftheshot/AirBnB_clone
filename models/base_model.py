7bf4630fd56a65b9cc2b204fadba573667ba0e4e
eliasyeme <eliasyeme@gmail.com>
2023-11-06 12:07:14 +0300

    Modified base model to accept arguments

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,17 @@ FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for k, v in kwargs.items():
+                if k in ("created_at", "updated_at"):
+                    self.__dict__[k] = datetime.strptime(v, FORMAT)
+                else:
+                    self.__dict__[k] = v
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
