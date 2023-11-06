9d3aab29f71366f48c45318c96612798c1ddaa00
eliasyeme <eliasyeme@gmail.com>
2023-11-06 11:25:59 +0300

    Added public instance attributes, __str__, save and to_dict methods to base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
+
 class BaseModel:
     def __init__(self):
-        pass
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        pass
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        pass
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        pass
+        return {
+            **self.__dict__,
+            "created_at": self.created_at.strftime(FORMAT),
+            "updated_at": self.updated_at.strftime(FORMAT),
+            "__class__": self.__class__.__name__,
+        }
